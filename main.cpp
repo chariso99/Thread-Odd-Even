446b70e068d38c4c05821e9697696d0b1904bbf4
chariso99 <102410959+chariso99@users.noreply.github.com>
2022-03-26 19:19:41 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+//untuk menambahkan delay pada thread.
+void delay(int t) {
+	this_thread::sleep_for(chrono::milliseconds(t));
+}
+
+//thread oddnumber / ganjil.
+void oddnumber(int x) {
+	for (int i = 1; i <= x; i++) {
+		if (i % 2 != 0) {
+			cout << "pesanan No." << i << " (Oddnumber)" << endl;
+			cout << "waktu delay 1 second" << endl << endl;
+			delay(1000);//delay 1 second
+		}
+	}
+}
+
+//thread evennumber / genap.
+void evennumber(int y) {
+	for (int i = 1; i <= y; i++) {
+		if (i % 2 == 0) {
+			cout << "pesanan No." << i << " (evennumber)" << endl;
+			cout << "waktu delay 2.5 seconds" << endl << endl;
+			delay(2500);//delay 2,5 second
+		}
+	}
+}
+
+int main() {
+	//untuk print odd dan even.
+	thread Charis(oddnumber,20);
+	thread Ali(evennumber,20);
+
+	//untuk synchronization thread.
+	//dan memastikan thread hingga selesai.
+	Charis.join();
+	Ali.join();
+
+	cout << "Semua pesanan sudah siap" << endl;
+	return 0;
+}
